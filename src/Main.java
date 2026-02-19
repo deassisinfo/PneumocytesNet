2695793a6547eacc20eff9ab357ef1e7f8de9041
deassisinfo <32418684+deassisinfo@users.noreply.github.com>
2026-02-18 21:16:00 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
@@ -19,10 +20,36 @@ public class Main {
 		Exec.run60 = false;
 		
 		//runs a single virtual experiment 
-		Exec.runSingle(Exec.NET_SCENARIO_1, Exec.IN_INFECTED, false);
+		//Exec.runSingle(Exec.NET_SCENARIO_1, Exec.IN_INFECTED, false);
+		
+		runValidationUnshuffled(false);
+		
+//		String baseFileName = "/Users/deassis/Documents/Projects/publications/Achyudhan/data/variance_test_full";
+//		File last15 = null;
+//		File lastiter1 = null;
+//		File lastiter0 = null;
+//		
+//		int[] idx = null;
+//		
+//		for(int i = 0; i < 400; i++) {
+//			last15 = new File(baseFileName + "_15_" + i + ".csv");
+//			lastiter1 = new File(baseFileName + "_last_" + i + ".csv");
+//			lastiter0 = new File(baseFileName + "_last_minus1_" + i + ".csv");
+//			
+//			idx = Exec.convergence(Exec.NET_SCENARIO_2, Exec.IN_INFECTED, 	  true, last15);
+//				  Exec.printSingle(Exec.NET_SCENARIO_2, Exec.IN_INFECTED, 30, idx,  lastiter1, lastiter0);
+//			
+//			if(i%10 == 0)System.out.println(i);
+//		}
+		
+//		Exec.convergence(Exec.NET_SCENARIO_2, Exec.IN_INFECTED, true, null);
+//		Exec.printSingle(Exec.NET_SCENARIO_2, Exec.IN_INFECTED, 15, true);
+//		for(double c : conv)
+//			System.out.println(c);
 		
 		//runs the validation
-//		runValidationUnshuffled();
+		//runValidationUnshuffled(false);
+//		runSirolimusReduced();
 		//runs the Random Networks (for validation)
 //		runValidationShuffled();
 		
@@ -35,6 +62,18 @@ public class Main {
 //		runPerturbation3();
 //		runPerturbation4();
 		
+		/**
+		 * # QN net running!
+56, 29, 9, 10, 8
+9181
+56, 29, 9, 10, 8
+9126
+56, 28, 9, 10, 9
+9034
+56, 26, 9, 10, 11
+8941
+		 */
+		
 		//runs the systematic edge perturbation analysis
 //		runSirolimusWithEdgePerturbation("1");
 //		runSirolimusWithEdgePerturbation("2");
@@ -45,7 +84,7 @@ public class Main {
 
 	
 	/* Tables 1, 2, and 4*/
-	private static void runValidationUnshuffled() throws FileNotFoundException {
+	private static void runValidationUnshuffled(boolean map) throws FileNotFoundException {
 		String name = "unshuffled_original"; //"rvalidation_fit_ifn";//"t2_experiment_fit_ifn";//"bvalidation_fit_ifn";//"experiments";
 		int iterations = 1;
 		boolean shuffle = false;
@@ -53,18 +92,20 @@ public class Main {
 		//Exec.validation(name, iterations, shuffle, inVivo);
 		
 		Exec.useFit(false);
+		Exec.useMap(map);
 		//name = "unshuffled_fit";
 		Exec.validation(name, iterations, shuffle, inVivo);
 	}
-	private static void runValidationShuffled() throws FileNotFoundException {
+	private static void runValidationShuffled(boolean map) throws FileNotFoundException {
 		String name = "shuffled_original"; //"rvalidation_fit_ifn";//"t2_experiment_fit_ifn";//"bvalidation_fit_ifn";//"experiments";
 		int iterations = 400;
 		boolean shuffle = true;
 		boolean inVivo = false;
 		Exec.validation(name, iterations, shuffle, inVivo);
 		
-		Exec.useFit(true);
-		name = "shuffled_fit";
+		Exec.useFit(false);
+		Exec.useMap(map);
+		name = "shuffled_unfit";
 		Exec.validation(name, iterations, shuffle, inVivo);
 	}
 	
@@ -132,6 +173,20 @@ public class Main {
 		Exec.runSirolimus(Exec.IN_INFECTED_SIROLIMUS, 100);
 	}
 
+	
+	private static void runSirolimusMap() {
+		Exec.useMap(true);
+		Exec.runSirolimus(Exec.IN_INFECTED, 100);
+		System.out.println("\n\n");
+		Exec.runSirolimus(Exec.IN_INFECTED_SIROLIMUS, 100);
+	}
+	
+	private static void runSirolimusReduced() {
+		Exec.useReduced();
+		Exec.runSirolimus(Exec.IN_INFECTED, 100);
+		System.out.println("\n\n");
+		Exec.runSirolimus(Exec.IN_INFECTED_SIROLIMUS, 100);
+	}
 	
 //	private static void runSirolimusFitKO() {
 //		/*Exec.useFit(true);
